4c3664b77f613c1a427457206fee94ffe37e439e
Jemtaly <Jemtaly@outlook.com>
2025-12-29 11:58:08 +0800

    add __version__ in __init__.py

--- src/pymcl/__init__.py
+++ src/pymcl/__init__.py
@@ -1,3 +1,14 @@
 from ._pymcl import *
 
-__all__ = ["Fr", "G1", "G2", "GT", "pairing", "r", "g1", "g2"]
+__all__ = [
+    "Fr",
+    "G1",
+    "G2",
+    "GT",
+    "r",
+    "g1",
+    "g2",
+    "pairing",
+]
+
+__version__ = "1.0.0"
